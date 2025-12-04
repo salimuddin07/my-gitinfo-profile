11dd371cec174b32ec85e4dc830234ce43a7197d
salimuddin07 <salimuddin07@users.noreply.github.com>
2025-12-04 19:42:21 +0530

    Refactor ReadmeCard component for improved layout and consistency

--- src/components/readme-card/index.tsx
+++ src/components/readme-card/index.tsx
@@ -17,33 +17,22 @@ const ReadmeCard: React.FC<ReadmeCardProps> = ({
 
   if (loading) {
     return (
-      <div className="card shadow-lg compact bg-base-100">
-        <div className="card-body">
-          <div className="mx-3">
-            <div className="mt-3">
-              {skeleton({ widthCls: 'w-full', heightCls: 'h-64' })}
-            </div>
-          </div>
+      <div className="card shadow-md card-sm bg-base-100">
+        <div className="flex justify-between flex-col p-8 h-full w-full">
+          {skeleton({ widthCls: 'w-full', heightCls: 'h-64' })}
         </div>
       </div>
     );
   }
 
   return (
-    <div className="card shadow-lg compact bg-base-100">
-      <div className="card-body">
-        <div className="mx-3">
-          <div className="mt-4">
-            <img 
-              src="https://i.pinimg.com/originals/a5/d1/f6/a5d1f61e89bdce061817723044f8a757.gif" 
-              alt="Cyberpunk Animation"
-              className="w-full h-auto rounded-lg"
-              style={{
-                display: 'block'
-              }}
-            />
-          </div>
-        </div>
+    <div className="card shadow-md card-sm bg-base-100">
+      <div className="flex justify-between flex-col p-8 h-full w-full">
+        <img 
+          src="https://i.pinimg.com/originals/a5/d1/f6/a5d1f61e89bdce061817723044f8a757.gif" 
+          alt="Cyberpunk Animation"
+          className="w-full h-auto rounded-lg"
+        />
       </div>
     </div>
   );
